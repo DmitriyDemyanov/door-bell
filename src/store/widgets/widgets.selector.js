69d1b6a8391c6fc8b802553eb3c83ef0953950fb
Hennadii <khlepitko.gennady@gmail.com>
2024-04-01 21:25:02 +0300

    vct-16 - storage variables naming improved vct-17 - widgets storage refactored

--- src/store/widgets/widgets.selector.js
+++ src/store/widgets/widgets.selector.js
@@ -1,14 +1,7 @@
-export const getWidgetsFooter = (state) => state.widgets.widgetsFooter;
-export const getWidgetsMain = (state) => state.widgets.widgetsMain;
+export const getFooterWidgets = (state) => state.widgets.footerWidgets;
 export const getAllSettingsItems = (state) => state.widgets.allSettings;
 export const getAllServicesItems = (state) => state.widgets.allService;
 
-export const getRenderWidgets = (state) => {
-  return state.widgets.widgetsMain.concat(state.widgets.renderWidgets,state.widgets.addButtonWidget)
-}
+export const getWidgetsList = (state) => state.widgets.widgetsList;
 
 export const getModalCommonInfo = (state) => state.widgets.modalCommonInfo;
-
-
-
-
