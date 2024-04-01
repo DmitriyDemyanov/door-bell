ea65912f8ff0b2935b167a51d24accf19600eb56
Hennadii <khlepitko.gennady@gmail.com>
2024-04-01 21:43:17 +0300

    vct-10 - calculation of rendered icon moved to computed property

--- src/components/widget-item/widget-item.component.jsx
+++ src/components/widget-item/widget-item.component.jsx
@@ -1,45 +1,51 @@
-import { useNavigate } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
+import {useNavigate}              from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 
-import { getWidgetsList } from '../../store/widgets/widgets.selector';
-import { setAddedWidgets } from '../../store/widgets/widgets.actions';
+import {getWidgetsList}  from "../../store/widgets/widgets.selector";
+import {setAddedWidgets} from "../../store/widgets/widgets.actions";
 
-import { setModalCommonInfo } from '../../store/widgets/widgets.actions';
+import {setModalCommonInfo} from "../../store/widgets/widgets.actions";
 
-import { ReactComponent as ChevronRightIcon } from '../../assets/widgets-icon/chevron-right-icon.svg';
-import { ReactComponent as CheckIcon } from '../../assets/widgets-icon/check-icon.svg';
-import { ReactComponent as PlusSmallIcon } from '../../assets/widgets-icon/plus-small-icon.svg';
-import { WidgetItemContainer } from "./widget-item.styles";
+import {ReactComponent as ChevronRightIcon} from "../../assets/widgets-icon/chevron-right-icon.svg";
+import {ReactComponent as CheckIcon}        from "../../assets/widgets-icon/check-icon.svg";
+import {ReactComponent as PlusSmallIcon}    from "../../assets/widgets-icon/plus-small-icon.svg";
+import {WidgetItemContainer}                from "./widget-item.styles";
 
+const WidgetItem = ({item}) => {
+    const {title, link} = item;
+    const nav           = useNavigate();
+    const widgetsList   = useSelector(getWidgetsList);
+    const dispatch      = useDispatch();
 
-const WidgetItem = ({ item }) => {
-  const { title,link } = item;
-  const nav = useNavigate();
-  const currentWidgetsRender = useSelector(getWidgetsList);
-  const dispatch = useDispatch();
-
-  const addWidget = (item) => {
-    if (link) {
-      return nav(link);
-    }
-    if (!currentWidgetsRender.find((el) => el.id === item.id)) {
-      dispatch(setAddedWidgets(item));
-      dispatch(setModalCommonInfo(item));
-    }
-  };
-
-  return (
-    <WidgetItemContainer onClick={() => addWidget(item)} >
-      <div className="widget-title"> {title} </div>
-      <div className="wrapper-bg"></div>
-      <div className="widget-icon">
-        {
-          link ? <ChevronRightIcon /> : (currentWidgetsRender.find((el) => el.title === title) ?
-            <div ><CheckIcon /></div> : <div ><PlusSmallIcon /></div>)
+    const addWidget = (item) => {
+        if (link) {
+            return nav(link);
         }
-      </div>
-    </WidgetItemContainer>
-  )
+        if (!widgetsList.find((el) => el.id === item.id)) {
+            dispatch(setAddedWidgets(item));
+            dispatch(setModalCommonInfo(item));
+        }
+    };
+
+    const renderedIcon = () => {
+        if (link) {
+            return <ChevronRightIcon/>;
+        }
+        if (widgetsList.find((el) => el.title === title)) {
+            return <div><CheckIcon/></div>;
+        }
+        return <div><PlusSmallIcon/></div>;
+    };
+
+    return (
+        <WidgetItemContainer onClick={() => addWidget(item)}>
+            <div className="widget-title"> {title} </div>
+            <div className="wrapper-bg"></div>
+            <div className="widget-icon">
+                {renderedIcon()}
+            </div>
+        </WidgetItemContainer>
+    );
 };
 
 export default WidgetItem;
