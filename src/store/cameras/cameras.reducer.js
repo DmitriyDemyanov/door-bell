48a2c302a2bccdd89b83c6e734f660bf5ebba006
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm startgit config --global user.email dimaprostokosmos@gmail.com>
2024-04-03 14:31:09 +0300

    fexed allCameras data

--- src/store/cameras/cameras.reducer.js
+++ src/store/cameras/cameras.reducer.js
@@ -3,32 +3,32 @@
 const INITIAL_STATE = {
   allCameras: [
     {
-      name: 'camera',
+      type: 'camera',
       title: 'Main Entrance camera',
       url: 'https://cdn.pixabay.com/photo/2023/03/06/20/35/fish-7834256_1280.jpg',
-      id: 'c-main-entrance',
-      date: '02.06.2023 | 12:34',
+      id: 'c-0001',
+      date: '1675679640000',
     },
     {
-      name: 'camera',
+      type: 'camera',
       title: 'Main Exit camera',
       url: 'https://cdn.pixabay.com/photo/2017/12/31/06/16/boats-3051610_1280.jpg',
-      id: 'c-exit-entrance',
-      date: '09.09.2022 | 19:23',
+      id: 'c-0002',
+      date: '1662740580000',
     },
     {
-      name: 'camera',
+      type: 'camera',
       title: 'Back Entrance camera',
       url: '',
-      id: 'c-back-entrance',
-      date: '09.12.2023 | 18:45',
+      id: 'c-0003',
+      date: '1694533500000',
     },
     {
-      name: 'camera',
+      type: 'camera',
       title: 'Window camera',
       url: 'https://cdn.pixabay.com/photo/2018/03/27/15/04/women-3266211_1280.jpg',
-      id: 'c-window',
-      date: '11.16.2022 | 19:05',
+      id: 'c-type',
+      date: '1668618300000',
     },
   ]
 };
@@ -36,12 +36,12 @@ const INITIAL_STATE = {
 export const camerasReducer = (state = INITIAL_STATE,action) => {
   const { type,payload } = action;
 
-  // switch (type) {
-  //   case   :
-  //   return {
-  //     ...state
-  //   }
-  // }
-  // default:
-  //   return state
+  switch (type) {
+  case '':
+  return {
+    ...state
+  }
+    default:
+return state
+  }
 }
