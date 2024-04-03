3b851867310065229da4b690b4f14bdd94094a1a
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm startgit config --global user.email dimaprostokosmos@gmail.com>
2024-04-03 10:56:33 +0300

    vct-12 is done

--- src/routes/message-screen/message-screen.styles.jsx
+++ src/routes/message-screen/message-screen.styles.jsx
@@ -1,32 +1,30 @@
 import styled from "styled-components";
 
 export const MessageScreenContainer = styled.div`
-  .remove-message{
+  .remove-message {
     position: absolute;
+    left: 24px;
+    right: 24px;
     bottom: 48px;
-    width: 84%;
     height: 104px;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    background: linear-gradient(to bottom, rgb(150,2,2), rgb(110,4,4));
     border-radius: 3px;
-    background: linear-gradient(180deg, #960202, #6e0404);
     font-size: 28px;
     font-weight: 700;
-    z-index: 100 !important;
-    .back-bg {
+    display: flex;
+    text-align: center;
+    align-items: center;
+    padding: 0 12px;
+
+    &:before {
+      content: "";
       position: absolute;
-      width: calc(100% + 2px);
-      height: 102px;
-      bottom: 3px;
-      left: -1px;
-      right: 0;
-      background:radial-gradient(ellipse at top, rgba(255, 255, 255, 0.4) 20%, transparent 90%);
-      border-radius: 3px;
-      z-index: -100 !important;
+      width: calc(100% + 4px);
+      left: -2px;
+      top: -2px;
+      height: calc(100% + 2px);
+      background: radial-gradient(ellipse at top, rgba(255, 255, 255, 0.4) 20%, transparent 90%);
+      z-index: -1;
     }
   }
-   
 `
