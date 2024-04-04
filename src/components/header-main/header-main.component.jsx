c5cad99d0b7bb720a9384991da279b9678811450
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm startgit config --global user.email dimaprostokosmos@gmail.com>
2024-04-04 21:57:20 +0300

    tried to set date  main screen

--- src/components/header-main/header-main.component.jsx
+++ src/components/header-main/header-main.component.jsx
@@ -1,13 +1,30 @@
-
+import { useEffect,useState } from 'react';
 import { ReactComponent as SettingsIcon } from '../../assets/icons-header/settings-icon.svg';
 
 import { HeaderMainContainer } from "./header-main.styles";
 
+
+console.log(new Date(Date.now()).toLocaleString('en',{ month: 'short',day: '2-digit',year: 'numeric' }));
+
 const HeaderMain = () => {
+  const [dateMain,setDate] = useState(new Date(Date.now()).toLocaleString('en',{ month: 'short',day: '2-digit',year: 'numeric' }));
+
+  const [timeMain,setTime] = useState(`${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}`);
+
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(`${new Date(Date.now()).getHours()}:${new Date(Date.now()).getMinutes()}`);
+      console.log('!!!!!')
+    },50000);
+  },[timeMain,])
+
+
+
 
   return (
     <HeaderMainContainer>
-      <div className='header-date' > 09:45 May 26, 2023 </div>
+      <div className='header-date'>{timeMain}  {dateMain} </div>
       <div className='wrapper-settings' onClick={() => console.log('/settings')} ><SettingsIcon /></div>
     </HeaderMainContainer>
   )
