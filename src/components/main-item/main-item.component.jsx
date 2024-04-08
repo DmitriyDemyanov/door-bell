cd4fc65945770e5328f645198bdbf52e8396ad5a
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm startgit config --global user.email dimaprostokosmos@gmail.com>
2024-04-08 12:25:49 +0300

    main cameras route added

--- src/components/main-item/main-item.component.jsx
+++ src/components/main-item/main-item.component.jsx
@@ -1,5 +1,7 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+
 import { getActiveMessage } from "../../store/messages/messages.selector";
+import { setDetailsCamera } from "../../store/cameras/cameras.action";
 
 import SvgIcon from "../icon-svg/svg-icon.component";
 
@@ -10,15 +12,25 @@ const typesDefaultIcon = {
     message: 'message-icon',
 }
 
+const links = {
+    camera: '/cameras/details-camera',
+    
+};
+
 const MainItem = ({ item }) => {
     const { icon,title,link,type } = item;
-    console.log('icon=>>>>',type);
-
+    const dispatch = useDispatch();
     const activeMessage = useSelector(getActiveMessage);
+
+    const handleClick = () => {
+        if (type === 'camera') {
+            dispatch(setDetailsCamera(item));
+        }
+    }
     return (
         <MainItemContainer
-            onClick={(event) => console.log('event',event)}
-            to={link}
+            onClick={() => handleClick()}
+            to={link ?? links[type]}
             className={`${title === "Leave" ? "accent-bg" : ""} `}>
             <div className={`wrapper-icon ${icon === "plus-icon" ? "center" : ""} `}>
                 <SvgIcon name={icon || typesDefaultIcon[type]} />
